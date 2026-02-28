b96f52bff92d9f6e1cd1a88989dfff7de8d72423
James Cruddas <james.cruddas@informed.com>
2026-02-28 20:09:55 +0000

    test: enforce 90% jest coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,16 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   collectCoverage: true,
   collectCoverageFrom: [
-    'assets/js/app.js',
     'assets/js/utilities.js'
   ],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90
+    }
+  },
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'json-summary'],
   setupFiles: ['<rootDir>/jest.setup.js'],
